1cc54baa233c45117d3964ffffdbbe8acf72aadf
Vasanthan <srvasanthan33@outlook.com>
2024-09-10 03:27:21 +0530

    ip-details added

--- desktop-app/src/components/HomePage/HomePage.jsx
+++ desktop-app/src/components/HomePage/HomePage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function HomePage() {
+  const [ipdetails,setIpdetails] = useState()
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      try{
+        const conn = await axios.get('https://ipinfo.io/json')
+        setIpdetails(conn.data)
+      console.log(conn.data)
+      
+      }
+      catch(err)
+      {
+        console.error('Error fetching data:', err);
+      }
+    }
+    
+    fetchData()
+  },[])
   return (
     <div className="content-container">
-      HomePage
+      {ipdetails?(<div className='ip-details'>
+        <h2 className="ip">{ipdetails.ip}</h2>
+        <h2 className="ip-city">{ipdetails.city}</h2>
+        <h2 className="ip-region">{ipdetails.region}</h2>
+        <h2 className="country">{ipdetails.country}</h2>
+      </div>):(<></>)}
+      
     </div>
   )
 }
